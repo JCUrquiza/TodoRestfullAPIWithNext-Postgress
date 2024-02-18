18546d03a9788e17077e38eb48df12ed3216f77c
Juan <gurquiza8@gmail.com>
2024-02-18 16:07:20 -0600

    Creating one todo

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server';
+import * as yup from 'yup';
 
 export async function GET(request: Request) {
 
@@ -24,5 +25,28 @@ export async function GET(request: Request) {
     return NextResponse.json({
         todos
     });
+
+}
+
+
+
+const postSchema = yup.object({
+    description: yup.string().required(),
+    complete: yup.boolean().optional().default(false)   // TODO: Interesting...
+})
+
+export async function POST(request: Request) {
+
+    try {
+            
+        const { complete, description } = await postSchema.validate(await request.json());
+        
+        const todo = await prisma?.todo.create({ data: { complete, description } });
+        
+        return NextResponse.json(todo);
+        
+    } catch (error) {
+        return NextResponse.json( error, { status: 400 } );
+    }
 
 }
