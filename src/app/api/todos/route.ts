4adeb057182b9f82885ca0eded6de0b0cddd4c00
Juan <gurquiza8@gmail.com>
2024-02-18 15:27:29 -0600

    Creating findMany with params take and skip

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -2,7 +2,24 @@ import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
 
-    const todos = await prisma?.todo.findMany();
+    const { searchParams } = new URL(request.url);
+    const take = searchParams.get('take') ?? '10';
+    const skip = searchParams.get('skip') ?? '0';
+    if ( isNaN(Number(take)) ) {
+        return NextResponse.json({
+            message: 'Take tiene que ser un número'
+        }, { status: 400 })
+    }
+    if ( isNaN(Number(skip)) ) {
+        return NextResponse.json({
+            message: 'Skip tiene que ser un número'
+        }, { status: 400 })
+    }
+
+    const todos = await prisma?.todo.findMany({
+        take: Number(take),
+        skip: Number(skip)
+    });
 
     return NextResponse.json({
         todos
