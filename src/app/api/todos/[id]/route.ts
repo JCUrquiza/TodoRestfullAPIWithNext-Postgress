8f0f6f326a13b2fa0dd1853a7cd33ee5cffc32b9
Juan <gurquiza8@gmail.com>
2024-02-18 19:00:07 -0600

    Finish all basic endpoints

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,4 +1,6 @@
+import { Todo } from '@prisma/client';
 import { NextResponse } from 'next/server';
+import * as yup from 'yup';
 
 interface Segments {
     params: {
@@ -18,8 +20,44 @@ export async function GET(request: Request, { params }: Segments) {
         }, { status: 404 });
     }
 
-    return NextResponse.json({
-        todo
-    });
+    return NextResponse.json(todo);
+
+}
+
+
+
+const putSchema = yup.object({
+    complete: yup.boolean().optional(),
+    description: yup.string().optional()
+})
+
+export async function PUT(request: Request, { params }: Segments) {
+
+    const { id } = params;
+
+    const todo = await prisma?.todo.findFirst({ where: { id: id } });
+
+    if ( !todo ) {
+        return NextResponse.json({
+            message: 'Todo no existe'
+        }, { status: 404 });
+    }
+
+    try {
+
+        const { complete, description, ...rest } = await putSchema.validate(await request.json());
+
+        const updatedTodo = await prisma?.todo.update({
+            where: { id },
+            data: { complete, description } 
+        })
+
+        return NextResponse.json(updatedTodo);
+        
+    } catch (error) {
+        return NextResponse.json({
+            message: error
+        }, {status: 400});
+    }
 
 }
