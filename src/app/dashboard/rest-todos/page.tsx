5661848c85cd1796bea03a484c5c733c2f296b7f
Juan <gurquiza8@gmail.com>
2024-02-19 17:05:26 -0600

    Add Todo carts

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import prisma from '@/lib/prisma';
+import { TodosGrid } from '@/todos';
 
-export default function RestTodosPage() {
+export default async function RestTodosPage() {
+
+    const todos = await prisma.todo.findMany({ orderBy: { description: 'asc' } });
+
     return (
-        <div>Rest Todos page</div>
+        <TodosGrid todos={todos} />
     )
 }
