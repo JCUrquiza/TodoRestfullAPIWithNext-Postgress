fee1ebab47e0ddfe7ec9204fa2b32c0b07db5481
Juan <gurquiza8@gmail.com>
2024-02-24 20:28:23 -0600

    Reading by array of products to show products

--- src/app/dashboard/products/page.tsx
+++ src/app/dashboard/products/page.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { ProductCard } from '@/productos';
+import { products } from '@/productos/data/products';
 
 export default function ProductsPage() {
     return (
         <div className='grid grid-cols-1 sm:grid-cols-3 gap-2'>
 
-            <ProductCard />
+            {
+                products.map( product => (
+                    <ProductCard key={product.id} {...product} />
+                ))
+            }
+
 
         </div>
     )
