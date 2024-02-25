a869b28de33347acc6b9a1d55d9e4c57c63f6a4b
Juan <gurquiza8@gmail.com>
2024-02-24 22:27:40 -0600

    Build a productincart page

--- src/componets/TopMenu.tsx
+++ src/componets/TopMenu.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { cookies } from 'next/headers';
+import Link from 'next/link';
 import { CiChat1, CiMenuBurger, CiSearch, CiShoppingBasket } from 'react-icons/ci';
 
 const getTotalCount = (cart: {[id: string]: number}) => {
@@ -43,12 +44,15 @@ export const TopMenu = () => {
                     <button className="flex items-center justify-center w-10 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
                         <CiChat1 size={25} />
                     </button>
-                    <button className="p-2 flex items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
+                    <Link 
+                        className="p-2 flex items-center justify-center h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200"
+                        href={'/dashboard/cart'}
+                    >
                         {
                             ( totalItems > 0 ) && (<span className='text-sm mr-2 text-blue-800 font-bold'>{ totalItems }</span>)
                         }
                         <CiShoppingBasket size={25} />
-                    </button>
+                    </Link>
                 </div>
             </div>
         </div>
