a90a075b3ff21374fb9868e52d13158684023d55
Juan <gurquiza8@gmail.com>
2024-02-26 13:30:22 -0600

    Show client info on Sidebar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,7 @@
-import { WidgetItem } from '@/componets';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../api/auth/[...nextauth]/route';
 import { redirect } from 'next/navigation';
+import { WidgetItem } from '@/componets';
+import { authOptions } from '../api/auth/[...nextauth]/route';
 
 export default async function DashboardPage() {
 
