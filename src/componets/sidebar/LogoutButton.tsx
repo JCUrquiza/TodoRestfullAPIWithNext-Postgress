5ef39bce782e55697d6bbcacd756d09fe97108d2
Juan <gurquiza8@gmail.com>
2024-02-26 14:25:14 -0600

    Configuring login and logaout

---
+++ src/componets/sidebar/LogoutButton.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import React from 'react';
+import { useSession, signIn, signOut } from 'next-auth/react';
+import { CiLogout } from 'react-icons/ci';
+import { IoShieldOutline } from 'react-icons/io5';
+
+export const LogoutButton = () => {
+
+    const { data: session, status } = useSession();
+
+    if ( status === 'loading' ) {
+        return (
+            <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
+                <IoShieldOutline />
+                <span className="group-hover:text-gray-700">Espere...</span>
+            </button>    
+        )
+    }
+
+    if ( status === 'unauthenticated' ) {
+        return (
+            <button 
+                className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
+                onClick={() => signIn()}
+            >
+                <IoShieldOutline />
+                <span className="group-hover:text-gray-700">Ingresar</span>
+            </button>    
+        )
+    }
+
+    return (
+        <button 
+            className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group"
+            onClick={() => signOut()}
+        >
+            <CiLogout />
+            <span className="group-hover:text-gray-700">Logout</span>
+        </button>
+    )
+}
--- src/componets/sidebar/Sidebar.tsx
+++ src/componets/sidebar/Sidebar.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { getServerSession } from 'next-auth';
-import { CiLogout } from 'react-icons/ci';
 import { IoBaseballOutline, IoCalendarOutline, IoCheckboxOutline, IoListOutline, IoNuclearOutline, IoPersonOutline } from 'react-icons/io5';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import { SidebarItem } from '.';
+import { LogoutButton, SidebarItem } from '..';
 
 const menuItems = [
     {
@@ -87,10 +86,7 @@ export const Sidebar = async() => {
             </div>
 
             <div className="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
-                <button className="px-4 py-3 flex items-center space-x-4 rounded-md text-gray-600 group">
-                    <CiLogout />
-                    <span className="group-hover:text-gray-700">Logout</span>
-                </button>
+                <LogoutButton />
             </div>
         </aside>
     )
